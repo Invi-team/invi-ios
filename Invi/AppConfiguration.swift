7fbdf79d12c4eb66d0486cf6d032a7bdfdadc64c
Marcin Mucha <marcin.mucha@schibsted.com>
2021-11-10 18:34:09 +0100

    IIOS-17 Create debug and production environments

--- Invi/AppConfiguration.swift
+++ Invi/AppConfiguration.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct AppConfiguration {
     let apiEnviroment: ApiEnvironment = .prod
+    let buildConfiguration: BuildConfiguration = .live
 }
 
 enum ApiEnvironment: String {
@@ -17,5 +18,30 @@ enum ApiEnvironment: String {
 
     var baseURL: URL {
         return URL(string: "https://\(rawValue).invi.click/api/v1/")!
+    }
+}
+
+enum BuildConfiguration: Equatable {
+    enum DeploymentEnvironment {
+        case testFlight, appStore, adHoc
+    }
+
+    case debug
+    case release(DeploymentEnvironment)
+
+    static var live: BuildConfiguration {
+        #if DEBUG
+            return .debug
+        #else
+            if Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") != nil {
+                return .release(.adHoc)
+            } else {
+                if Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt" {
+                    return .release(.testFlight)
+                } else {
+                    return .release(.appStore)
+                }
+            }
+        #endif
     }
 }
