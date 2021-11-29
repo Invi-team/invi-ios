961079e7eabebc98f269a2aead657e01c12eda94
kuba <kuba@withplum.com>
2021-11-29 18:24:54 +0100

    Removed stupid changes

--- Invi/Dependencies/Application.swift
+++ Invi/Dependencies/Application.swift
@@ -9,19 +9,10 @@ import Foundation
 import UIKit
 
 struct Application {
-    var canOpenUrl: (URL) -> Bool = { url in
+    let canOpenUrl: (URL) -> Bool = { url in
         UIApplication.shared.canOpenURL(url)
     }
-    var openUrl: (URL) -> Void = { url in
+    let openUrl: (URL) -> Void = { url in
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-    
-    static let live = Application(
-        canOpenUrl: { url in
-            UIApplication.shared.canOpenURL(url)
-        },
-        openUrl: { url in
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        }
-    )
 }
